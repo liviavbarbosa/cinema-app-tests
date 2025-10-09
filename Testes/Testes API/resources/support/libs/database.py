33174f551ce5c1e78b6fbe528c86585c1b584748
Lívia Barbosa <liviabarbosa.aluno@unipampa.edu.br>
2025-10-09 17:02:09 -0300

    Adição dos testes de usuários de alta prioridade finalizados

--- Testes/Testes API/resources/support/libs/database.py
+++ Testes/Testes API/resources/support/libs/database.py
@@ -45,6 +45,13 @@ def clean_user(user):
         users.delete_many({"email": user_email})
 
 
+@keyword('Pegar id do usuario')
+def get_user_id(user):
+    users = db["users"]
+    user = users.find_one({"email": user["email"]})
+    return user["_id"] 
+
+
 @keyword('Remover filme do database')
 def remove_movie(id_movie):
     movies = db["movies"]
@@ -70,8 +77,8 @@ def insert_movie(movie):
 @keyword('Pegar id do filme')
 def get_movie_id(movie):
     movies = db["movies"]
-    id_movie = movies.find_one({"title": movie["title"]})
-    return id_movie["_id"] 
+    movie = movies.find_one({"title": movie["title"]})
+    return movie["_id"] 
 
 
 @keyword('Remover teatro do database')
@@ -94,8 +101,8 @@ def insert_theater(theater):
 @keyword('Pegar id do teatro')
 def get_theater_id(theater):
     theaters = db["theaters"]
-    id_theater = theaters.find_one({"name": theater["name"]})
-    return id_theater["_id"] 
+    theater = theaters.find_one({"name": theater["name"]})
+    return theater["_id"] 
 
 
 @keyword('Remover sessao do database')
@@ -106,12 +113,22 @@ def remove_session(id_session):
 
 @keyword('Inserir sessao no database')
 def insert_session(session):
+    seats = []
+    for row in range(8):  
+        row_letter = chr(ord('A') + row)
+        for num in range(1, 11):  
+            seats.append({
+                "row": row_letter,
+                "number": num,
+                "status": "available"
+            })
     doc = {
         "movie": session["movie"],
         "theater": session["theater"],
         "datetime": session["datetime"],
         "fullPrice": session["fullPrice"],
-        "halfPrice": session["halfPrice"]
+        "halfPrice": session["halfPrice"],
+        "seats": seats
     }
     sessions = db['sessions']
     sessions.insert_one(doc)
@@ -123,9 +140,37 @@ def get_session_id(session):
 
     movie_id = ObjectId(str(session["movie"]))
     theater_id = ObjectId(str(session["theater"]))
-    
-    session_doc = sessions.find_one({
+
+    doc = sessions.find_one({
         "movie": movie_id,
         "theater": theater_id
     })
-    return session_doc["_id"] 
+    return doc["_id"] 
+
+
+@keyword('Remover reserva do database')
+def remove_reservation(id_reservation):
+    reservations = db["reservations"]
+    reservations.delete_many({'_id': id_reservation}) 
+
+
+@keyword('Inserir reserva no database')
+def insert_reservation(reservation):
+    doc = {
+        "session": reservation["session"],
+        "seats": reservation["seats"],
+        "paymentMethod": reservation["paymentMethod"]
+    }
+    reservations = db['reservations']
+    reservations.insert_one(doc) 
+
+
+@keyword('Pegar id da reserva')
+def get_reservation_id(reservation):
+    reservations = db["reservations"]
+    session_id = ObjectId(str(reservation["session"]))
+    
+    doc = reservations.find_one({
+        "session": session_id
+    })
+    return doc["_id"]
